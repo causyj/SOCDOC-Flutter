463cb0cbcae7becf9109f06da734c189c405bfc0
yymin1022 <yymin1022@gmail.com>
2023-11-05 01:53:06 +0900

    Update SearchPage : Added Button for DetailPage Open

--- lib/Pages/SearchPage.dart
+++ lib/Pages/SearchPage.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'package:socdoc_flutter/Pages/DetailPage.dart';
+
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Text("Search Page")
+        child: Column(
+          children: [
+            const Text("Search Page"),
+            ElevatedButton(
+              child: const Text("Detail Page"),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage()));
+              }
+            )
+          ]
+        )
       )
     );
   }
