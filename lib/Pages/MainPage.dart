ab6beba68c5e9668122ee1d1a04e710846c4d762
yymin1022 <yymin1022@gmail.com>
2023-11-05 01:10:27 +0900

    Update MainPage.dart : Added Basic Elements

--- lib/Pages/MainPage.dart
+++ lib/Pages/MainPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:socdoc_flutter/Pages/MainSubPages/HomePage.dart';
+import 'package:socdoc_flutter/Pages/MainSubPages/SettingPage.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -8,9 +10,37 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final _pageList = [SettingPage(), HomePage()];
+  int _pageIdx = 1;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: SafeArea(
+        child: _pageList[_pageIdx],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _pageIdx,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search"
+          )
+        ],
+        onTap: (int idx) {
+          setState(() {
+            _pageIdx = idx;
+          });
+        },
+      ),
+    );
   }
 }
