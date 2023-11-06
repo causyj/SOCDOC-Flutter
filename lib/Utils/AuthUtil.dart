5a094d51bd7ce46b9b8caf750e0d7351e0a4b277
yymin1022 <yymin1022@gmail.com>
2023-11-06 22:43:54 +0900

    Update AuthUtil : Added Function for Apple Login Attempt

--- lib/Utils/AuthUtil.dart
+++ lib/Utils/AuthUtil.dart
@@ -1,6 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+Future<void> tryAppleLogin() async {
+  final appleProvider = AppleAuthProvider();
+  await FirebaseAuth.instance.signInWithProvider(appleProvider);
+}
+
 Future<void> tryGoogleLogin() async {
   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
   final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
