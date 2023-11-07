75750220a844f19bc5ad2e657ca6de10ab8f3133
pjw-redt <pjiww4@gmail.com>
2023-11-07 16:13:55 +0900

    nearbyPharmacy Card 단일 컴포넌트 완성

--- lib/Pages/DetailPage.dart
+++ lib/Pages/DetailPage.dart
@@ -6,17 +6,55 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final edgeInsets = EdgeInsets.only(left: 16.0, top: 5.0);
-    final textStyle = TextStyle(fontSize: 16);
+    final detailTextStyle = TextStyle(fontSize: 16);
+    final detailPharmacyStyle = TextStyle(fontSize: 17, fontWeight: FontWeight.bold);
+    final titlePharmacy = TextStyle(fontSize: 22, fontWeight: FontWeight.bold);
 
-    Widget buildRowWithIcon(IconData icon, String text) {
+    Widget detailHospital(IconData icon, String text) {
       return Row(
         children: [
           Padding(padding: edgeInsets, child: Icon(icon)),
           Padding(padding: EdgeInsets.only(left: 10.0, top: 5.0)),
-          Text(text, style: textStyle),
+          Text(text, style: detailTextStyle),
         ],
       );
     }
+
+    Widget detailPharmacy(String text) {
+      return Row(
+        children: [
+          Padding(padding: edgeInsets, child: Icon(Icons.location_on)),
+          Padding(padding: EdgeInsets.only(left: 10.0)),
+          Text(text, style: detailPharmacyStyle),
+        ],
+      );
+    }
+
+    Widget nearbyPharmacy(String text) {
+      return SizedBox(
+        height: 110, width: 350,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          elevation: 4.0,
+          surfaceTintColor: Colors.transparent,
+          color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 15.0, bottom: 5.0),
+                child: Text(text, style: titlePharmacy),
+              ),
+              detailPharmacy("동작구 상도동 4길 36"),
+            ],
+          ),
+        ),
+      );
+    }
+
+
 
     return DefaultTabController(
       length: 2,
@@ -57,10 +95,10 @@ class DetailPage extends StatelessWidget {
                       ],
                     ),
                     SizedBox(height: 10.0),
-                    buildRowWithIcon(Icons.call, "02-1234-5678"),
-                    buildRowWithIcon(Icons.location_on, "동작구 상도동 4길 36"),
-                    buildRowWithIcon(Icons.subway, "상도역 5번 출구"),
-                    buildRowWithIcon(Icons.alarm, "진료 시간"),
+                    detailHospital(Icons.call, "02-1234-5678"),
+                    detailHospital(Icons.location_on, "동작구 상도동 4길 36"),
+                    detailHospital(Icons.subway, "상도역 5번 출구"),
+                    detailHospital(Icons.alarm, "진료 시간"),
                   ],
                 ),
               ),
@@ -76,9 +114,9 @@ class DetailPage extends StatelessWidget {
                 child: TabBarView(
                   children: [
                     // 첫 번째 탭의 내용
-                    Center(child: Text("리뷰 내용")),
+                    Center(child: Text("리뷰")),
                     // 두 번째 탭의 내용
-                    Center(child: Text("주변 약국 내용")),
+                    Center(child: nearbyPharmacy("상도 온누리 약국")),
                   ],
                 ),
               ),
