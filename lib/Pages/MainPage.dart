a27e1e2a34aabfe3b84676744ff8d8ec1116d5bd
yymin1022 <yymin1022@gmail.com>
2023-11-10 20:59:39 +0900

    Update MainPage : Fixed some Stated for Applying Custom BNB

--- lib/Pages/MainPage.dart
+++ lib/Pages/MainPage.dart
@@ -2,51 +2,26 @@ import 'package:flutter/material.dart';
 
 import 'package:socdoc_flutter/Pages/MainSubPages/HomePage.dart';
 import 'package:socdoc_flutter/Pages/MainSubPages/SettingPage.dart';
-import 'package:socdoc_flutter/Pages/SearchPage.dart';
+import 'package:socdoc_flutter/Widgets/SocdocBottomNav.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
-  State<StatefulWidget> createState() => _MainPageState();
+  State<StatefulWidget> createState() => MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class MainPageState extends State<MainPage> {
   final _pageList = [SizedBox.shrink(), HomePage(), SettingPage()];
-  int _pageIdx = 1;
+  int pageIdx = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: _pageList[_pageIdx],
+        child: _pageList[pageIdx],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _pageIdx,
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: "Search"
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home"
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "My Page"
-          )
-        ],
-        onTap: (int idx) {
-          setState(() {
-            if(idx == 0){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => SearchPage()));
-            }else{
-              _pageIdx = idx;
-            }
-          });
-        },
-      ),
+      bottomNavigationBar: SocdocBottomNav()
     );
   }
 }
