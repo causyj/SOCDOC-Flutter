a477a2b2eafc95ca458366d26cd6190e82ba54c3
yymin1022 <yymin1022@gmail.com>
2023-11-10 21:10:13 +0900

    Update SocdocBottomNav : Added Basic BNB Functions

--- lib/Widgets/SocdocBottomNav.dart
+++ lib/Widgets/SocdocBottomNav.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:socdoc_flutter/Pages/MainPage.dart';
+import 'package:socdoc_flutter/Pages/SearchPage.dart';
+import 'package:socdoc_flutter/main.dart';
 
 class SocdocBottomNav extends StatefulWidget {
   const SocdocBottomNav({super.key});
@@ -10,9 +12,51 @@ class SocdocBottomNav extends StatefulWidget {
 }
 
 class SocdocBottomNavState extends State<SocdocBottomNav> {
+  var mainPage;
   @override
   Widget build(BuildContext context) {
-    var mainPage = context.findAncestorStateOfType<MainPageState>();
-    return Text("SOCDOC Bottom Nav");
+    mainPage = context.findAncestorStateOfType<MainPageState>()!;
+    return SizedBox(
+      height: 60,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          mainPage.pageIdx == 0
+              ? IconButton(
+                  onPressed: (){openSearch();},
+                  icon: const Icon(Icons.search))
+              : IconButton(
+                  onPressed: (){openSearch();},
+                  icon: const Icon(Icons.search)),
+          mainPage.pageIdx == 1
+              ? IconButton(
+                  onPressed: (){updateIdx(1);},
+                  icon: const Icon(Icons.home))
+              : IconButton(
+                  onPressed: (){updateIdx(1);},
+                  icon: const Icon(Icons.home)),
+          mainPage.pageIdx == 2
+              ? IconButton(
+                  onPressed: (){updateIdx(2);},
+                  icon: const Icon(Icons.person))
+              : IconButton(
+                  onPressed: (){updateIdx(2);},
+                  icon: const Icon(Icons.person))
+        ],
+      ),
+    );
+  }
+
+  void openSearch(){
+    Navigator.push(context,
+      MaterialPageRoute(
+        builder: (context) => SearchPage()
+      ));
+  }
+
+  void updateIdx(idx){
+    mainPage!.setState((){
+      mainPage.pageIdx = idx;
+    });
   }
 }
