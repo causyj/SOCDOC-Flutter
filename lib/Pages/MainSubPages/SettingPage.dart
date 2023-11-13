53892acbab977b5757de8cdee78d889018f9e875
yymin1022 <yymin1022@gmail.com>
2023-11-14 03:01:53 +0900

    Update Setting Page : Improved Logout Function Call Logic

--- lib/Pages/MainSubPages/SettingPage.dart
+++ lib/Pages/MainSubPages/SettingPage.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 
 import 'package:socdoc_flutter/Utils/AuthUtil.dart';
+import 'package:socdoc_flutter/main.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SocdocAppState socdocApp = context.findAncestorStateOfType<SocdocAppState>()!;
+
     return(
       Column(
         children: [
           const Text("Setting Page"),
           ElevatedButton(
             onPressed: (){
-              tryLogout();
+              tryFirebaseLogout(socdocApp);
             },
             child: const Text("Logout User")
           )
         ]
       )
-
     );
+  }
+
+  void tryFirebaseLogout(SocdocAppState socdocApp) async {
+    if(await tryLogout()){
+      socdocApp.setState(() {
+        socdocApp.isLoggedIn = false;
+      });
+    }
   }
 }
