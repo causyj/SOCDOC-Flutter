4383dbdcde9b838419a997beb100b241db657435
pjw-redt <pjiww4@gmail.com>
2023-11-15 21:58:12 +0900

    mypage 유저  컨테이너 제작

--- lib/Pages/MainSubPages/SettingPage.dart
+++ lib/Pages/MainSubPages/SettingPage.dart
@@ -13,18 +13,52 @@ class SettingPage extends StatelessWidget {
     return(
       Column(
         children: [
-          const Text("Setting Page"),
+          Container(
+            padding: EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Row(
+              children: [
+                // 왼쪽 이미지
+                Container(
+                  width: 50.0, // 이미지 너비 조절
+                  height: 50.0, // 이미지 높이 조절
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('/socdoc_logo.png'), // 이미지 경로 추가
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 16.0), // 이미지와 텍스트 간격 조절
+                // 오른쪽 텍스트
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '박지우님',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text('서울특별시 동작구'),
+                  ],
+                ),
+              ],
+            ),
+          ),
           ElevatedButton(
             onPressed: (){
               tryFirebaseLogout(socdocApp);
             },
-            child: const Text("Logout User")
+            child: const Text("로그아웃")
           ),
           ElevatedButton(
               onPressed: (){
                 tryFirebaseDeleteUser(socdocApp);
               },
-              child: const Text("Delete User")
+              child: const Text("회원 탈퇴")
           )
         ]
       )
@@ -34,7 +68,7 @@ class SettingPage extends StatelessWidget {
   void tryFirebaseLogout(SocdocAppState socdocApp) async {
     if(await tryLogout()){
       socdocApp.setState(() {
-        socdocApp.isLoggedIn = false;
+        //socdocApp.isLoggedIn = false;
       });
     }
   }
@@ -42,7 +76,7 @@ class SettingPage extends StatelessWidget {
   void tryFirebaseDeleteUser(SocdocAppState socdocApp) async {
     if(await tryDeleteUser()){
       socdocApp.setState(() {
-        socdocApp.isLoggedIn = false;
+        //socdocApp.isLoggedIn = false;
       });
     }
   }
