6c3eeb3cc4eb22a8457ae74745969c04d95d4b17
pjw-redt <pjiww4@gmail.com>
2023-11-16 14:16:19 +0900

    refactoring container

--- lib/Pages/MainSubPages/SettingPage.dart
+++ lib/Pages/MainSubPages/SettingPage.dart
@@ -1,72 +1,96 @@
 import 'package:flutter/material.dart';
-
 import 'package:socdoc_flutter/Utils/AuthUtil.dart';
 import 'package:socdoc_flutter/main.dart';
 
 class SettingPage extends StatelessWidget {
-  const SettingPage({super.key});
+  const SettingPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     SocdocAppState socdocApp = context.findAncestorStateOfType<SocdocAppState>()!;
 
-    return(
-      Column(
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
         children: [
-          Container(
-            padding: EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Row(
-              children: [
-                // 왼쪽 이미지
-                Container(
-                  width: 50.0, // 이미지 너비 조절
-                  height: 50.0, // 이미지 높이 조절
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage('assets/user.png'), // 이미지 경로 추가
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                SizedBox(width: 16.0), // 이미지와 텍스트 간격 조절
-                // 오른쪽 텍스트
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '박지우님',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text('서울특별시 동작구'),
-                  ],
-                ),
-              ],
-            ),
-          ),
+          buildUserInfoContainer(),
           ElevatedButton(
-            onPressed: (){
+            onPressed: () {
               tryFirebaseLogout(socdocApp);
             },
-            child: const Text("로그아웃")
+            child: const Text("로그아웃"),
           ),
           ElevatedButton(
-              onPressed: (){
-                tryFirebaseDeleteUser(socdocApp);
-              },
-              child: const Text("회원 탈퇴")
-          )
-        ]
-      )
+            onPressed: () {
+              tryFirebaseDeleteUser(socdocApp);
+            },
+            child: const Text("회원 탈퇴"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildUserInfoContainer() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 0,
+            blurRadius: 2,
+            offset: Offset(0, 5),
+          ),
+        ],
+        color: Colors.white,
+      ),
+      child: buildUserInfoRow(),
+    );
+  }
+
+  Widget buildUserInfoRow() {
+    return Row(
+      children: [
+        SizedBox(width: 10.0),
+        buildUserAvatar(),
+        SizedBox(width: 20.0),
+        buildUserInfoColumn(),
+      ],
+    );
+  }
+
+  Widget buildUserAvatar() {
+    return Container(
+      width: 60.0,
+      height: 60.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          image: AssetImage('assets/user.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget buildUserInfoColumn() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '사용자 님',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+        ),
+        SizedBox(height: 2.0),
+        Text('서울특별시 동작구', style: TextStyle(color: Colors.grey)),
+      ],
     );
   }
 
   void tryFirebaseLogout(SocdocAppState socdocApp) async {
-    if(await tryLogout()){
+    if (await tryLogout()) {
       socdocApp.setState(() {
         //socdocApp.isLoggedIn = false;
       });
@@ -74,7 +98,7 @@ class SettingPage extends StatelessWidget {
   }
 
   void tryFirebaseDeleteUser(SocdocAppState socdocApp) async {
-    if(await tryDeleteUser()){
+    if (await tryDeleteUser()) {
       socdocApp.setState(() {
         //socdocApp.isLoggedIn = false;
       });
