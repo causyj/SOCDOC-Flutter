551668be294d33c1fb28102b47384b9e81f73dd3
pjw-redt <pjiww4@gmail.com>
2023-11-24 19:22:03 +0900

    즐겨찾기 병원 목록 custom

--- lib/Pages/MainSubPages/SettingPage.dart
+++ lib/Pages/MainSubPages/SettingPage.dart
@@ -79,22 +79,44 @@ class SettingPage extends StatelessWidget {
     );
   }
 
-  Widget HospitalInfo(String name, String address, String img){
+  Widget HospitalInfo(String name, String address, String img) {
     return Container(
-      child: Column(
-        children: [
-          Image(image: AssetImage(img), width: 110, height: 110),
-          Text(name, style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold, color: AppColor.SocdocBlue)),
-          Row(
-            children: [
-              Icon(Icons.location_on, size: 15),
-              Text(address, style: TextStyle(fontSize: 13.0, color: Colors.black54)),
-            ],
-          ),
-        ]
-      )
+      width: 160,
+      height: 135,
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColor.SocdocBlue, width: 0.5),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10.0),
+          topRight: Radius.circular(10.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(image: AssetImage(img), width: 160, height: 85, fit: BoxFit.cover),
+            Padding(
+              padding: const EdgeInsets.only(left: 5.0, top: 5.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start, // 좌측 정렬을 위해 추가
+                children: [
+                  Text(name, style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold, color: AppColor.SocdocBlue)),
+                  Row(
+                    children: [
+                      Icon(Icons.location_on, size: 15),
+                      Text(address, style: TextStyle(fontSize: 13.0, color: Colors.black54)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
+
 
   Widget favoriteHospital(){
     return Container(
@@ -102,6 +124,7 @@ class SettingPage extends StatelessWidget {
       child: Row(
         children: [
           HospitalInfo("흑석성모안과의원", "동작구 상도로 36길", 'assets/hospital2.png'),
+          HospitalInfo("연세이비인후과", "동작구 상도로 17길", 'assets/hospital3.png')
         ],
       ),
     );
