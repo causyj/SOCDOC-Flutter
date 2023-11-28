8aca498352f386ae2512aa15e9176b379f07d32f
yymin1022 <yymin1022@gmail.com>
2023-11-28 17:33:58 +0900

    Update SearchPage : Applied Current Location Map Render

--- lib/Pages/SearchPage.dart
+++ lib/Pages/SearchPage.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:socdoc_flutter/Pages/DetailPage.dart';
@@ -36,22 +37,60 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
-  final Completer<GoogleMapController> _controller =
-  Completer<GoogleMapController>();
+  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
 
-  static const CameraPosition tmpCoord = CameraPosition(
+  static const CameraPosition initCoord = CameraPosition(
     target: LatLng(37.4905987, 126.9441426),
     zoom: 17,
   );
 
   @override
   Widget build(BuildContext context) {
+    _determinePosition();
+
     return GoogleMap(
       mapType: MapType.normal,
-      initialCameraPosition: tmpCoord,
+      initialCameraPosition: initCoord,
       onMapCreated: (GoogleMapController controller) {
         _controller.complete(controller);
       }
     );
+  }
+
+  Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    await Geolocator.getCurrentPosition().then((cur) => {
+      _moveCamera(cur.latitude, cur.longitude)
+    });
+  }
+
+  Future<void> _moveCamera(lat, lng) async {
+    CameraPosition newCoord = CameraPosition(
+      target: LatLng(lat, lng),
+      zoom: 17,
+    );
+
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(newCoord));
   }
 }
