6a0b13611df44b96bf49c9c8b0dd0b8616022154
pjw-redt <pjiww4@gmail.com>
2023-12-03 19:16:46 +0900

    onBoardingPage lottie setting 완료

--- lib/Pages/OnBoardingPage.dart
+++ lib/Pages/OnBoardingPage.dart
@@ -2,15 +2,87 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
 class OnBoardingPage extends StatefulWidget {
+  const OnBoardingPage({super.key});
+
   @override
   _OnBoardingPageState createState() => _OnBoardingPageState();
 }
 
 class _OnBoardingPageState extends State<OnBoardingPage> {
+
+  final animations = [
+    "assets/lottie/animation_1.json",
+    "assets/lottie/animation_2.json",
+    "assets/lottie/animation_3.json",
+  ];
+
+  final description_title = [
+    "Find",
+    "Reviews",
+    "Exploring",
+  ];
+
+  final description_info = [
+    "hospitals near me by distinct",
+    "from real users",
+    "surrounding pharmacies",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Lottie.asset('assets/lottie/animation_1.json'),
+        body: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                itemBuilder: (context, index) {
+                  return _PageLayout(
+                    animation: animations[index],
+                    title: description_title[index],
+                    info: description_info[index],
+                  );
+                },
+              ),
+            ),
+          ],
+        )
+    );
+  }
+}
+
+class _PageLayout extends StatelessWidget{
+  const _PageLayout({
+    required this.animation,
+    required this.title,
+    required this.info,
+  });
+
+  final String animation;
+  final String title;
+  final String info;
+
+  @override
+  Widget build(BuildContext context){
+    return Padding(padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height*0.7,
+              width: MediaQuery.of(context).size.width,
+              child: Lottie.asset(
+                animation,
+              ),
+            ),
+            Text(title, style: Theme.of(context).textTheme.headlineLarge,
+            textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 16),
+            Text(info, style: Theme.of(context).textTheme.headlineSmall,
+              textAlign: TextAlign.center,
+              maxLines: 2,
+            ),
+          ],
+        )
     );
   }
 }
