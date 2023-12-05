f711b825d6bb7046cb1e7d4a7d9a1431fd105e10
yymin1022 <yymin1022@gmail.com>
2023-12-06 03:56:33 +0900

    Update main.dart : Applied .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:socdoc_flutter/Pages/LoginPage.dart';
 import 'package:socdoc_flutter/Pages/MainPage.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const SocdocApp());
 }
 
