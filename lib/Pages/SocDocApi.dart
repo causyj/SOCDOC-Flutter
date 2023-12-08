6bff7ff097fda7bd449c76e97f1fa6207fb3d676
pjw-redt <pjiww4@gmail.com>
2023-12-08 16:21:36 +0900

    Dto 삭제한 버전의 병원 상세 정보 조회 API

--- lib/Pages/SocDocApi.dart
+++ lib/Pages/SocDocApi.dart
@@ -3,14 +3,14 @@ import 'package:http/http.dart' as http;
 import 'package:firebase_auth/firebase_auth.dart';
 
 class SocDocApi {
-  static Future<Map<String, dynamic>> fetchHospitalDetail() async {
+  static Future<dynamic> fetchHospitalDetail() async {
     var url = Uri.parse("https://socdoc.dev-lr.com/api/hospital/detail?hospitalId=A1100001&userId=${FirebaseAuth.instance.currentUser!.uid}");
     var response = await http.get(url);
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
 
     if (response.statusCode == 200) {
-      return jsonDecode(utf8.decode(response.bodyBytes));
+      return jsonDecode(utf8.decode(response.bodyBytes))["data"];
     } else {
       print("에러 발생: ${response.statusCode}");
       print("에러 내용: ${response.body}");
